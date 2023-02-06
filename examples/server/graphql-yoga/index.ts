33eef31dcf917a1d2620f9f312849ec8d83251d0
enisdenjo <badurinadenis@gmail.com>
2023-02-06 10:21:27 +0100

    yoga

---
+++ examples/server/graphql-yoga/index.ts
@@ -0,0 +1,14 @@
+import { createServer } from 'node:http';
+import { createYoga } from 'graphql-yoga';
+import { schema } from '<database>/schema.ts';
+
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({ schema });
+
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga);
+
+// Start the server and you're done!
+server.listen(50005, () => {
+  console.info('Server is running on http://localhost:50005/graphql');
+});
