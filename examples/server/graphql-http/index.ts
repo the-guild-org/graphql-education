76f6140c70cb59e1a1ad82bb0d46a3d5f4d7e14e
enisdenjo <badurinadenis@gmail.com>
2023-02-06 11:42:26 +0100

    graphql-http

---
+++ examples/server/graphql-http/index.ts
@@ -0,0 +1,18 @@
+import { createServer } from 'node:http';
+import { createHandler } from 'graphql-http/lib/use/node';
+import { schema, createContext } from '<database>/schema.ts';
+
+// Create the GraphQL over HTTP Node request handler
+const handler = createHandler({ schema, context: createContext });
+
+// Create a HTTP server using the listner on `/graphql`
+const server = createServer((req, res) => {
+  if (req.url?.startsWith('/graphql')) {
+    handler(req, res);
+  } else {
+    res.writeHead(404).end();
+  }
+});
+
+server.listen(50005);
+console.info('Server is running on http://localhost:50005/graphql');
