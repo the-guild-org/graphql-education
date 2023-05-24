a5a8bcfe7fce9fc6efbf4927e91b1855c07e84e6
enisdenjo <badurinadenis@gmail.com>
2023-05-24 12:42:07 +0200

    update apollo-server

--- examples/server/apollo-server/index.ts
+++ examples/server/apollo-server/index.ts
@@ -1,4 +1,5 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 import { sessionIdFromCookie, sessionIdToCookie } from '@server/utils';
 import {
   buildSchema,
@@ -8,6 +9,10 @@ import {
 (async () => {
   const server = new ApolloServer({
     schema: await buildSchema(),
+  });
+
+  await startStandaloneServer(server, {
+    listen: { port: 50005 },
     context: ({ req, res }) =>
       createContext({
         sessionId: sessionIdFromCookie(req.headers.cookie),
@@ -16,8 +21,6 @@ import {
         },
       }),
   });
-
-  await server.listen(50005);
 
   console.info('Server is running on http://localhost:50005/graphql');
 })();
