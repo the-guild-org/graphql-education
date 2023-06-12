7fbdda4c30bb30413da60529cba80eae57339d9a
enisdenjo <badurinadenis@gmail.com>
2023-06-12 12:29:03 +0200

    mercurius

--- examples/server/mercurius/authentication.ts
+++ examples/server/mercurius/authentication.ts
@@ -1,22 +1,20 @@
 import Fastify from 'fastify';
 import mercurius from 'mercurius';
 import { sessionIdFromCookie, sessionIdToCookie } from '@server/common';
-import {
-  buildSchema,
-  createContext,
-} from '@database/postgres-with-prisma/schema';
+import { Context } from '@schema/authentication';
+import { buildSchema } from '@database/mongodb/schema/authentication';
 
 const app = Fastify();
 
 app.register(mercurius, {
   schema: await buildSchema(),
   context: (req, reply) =>
-    createContext({
+    ({
       sessionId: sessionIdFromCookie(req.headers['cookie']),
       setSessionId(sessionId) {
         reply.header('set-cookie', sessionIdToCookie(sessionId));
       },
-    }),
+    } satisfies Context),
 });
 
 app.listen({ port: 50005 });
